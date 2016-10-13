6b032b4dae4a927b74fe3c1c8cbeb67e3000d027
Sara Sailors <sarasailors@gmail.com>
2016-10-13 16:49:33 -0400

    feat(mysql): continuing to work on mysql, etc

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var http = require('http');
+var database = require('./db/dbmodels');
 
 var app = express();
+app.set('port', process.env.PORT || 8000);
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
 app.use(express.static(__dirname + '/../client'));
 
-var server = app.listen(8000, function () {
-    console.log('Server listening on port ' + server.address().port);
+
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
+
+module.exports = app;
