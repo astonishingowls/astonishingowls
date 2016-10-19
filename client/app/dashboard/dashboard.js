34f74cde5469e9f73666715a3230e1c675a065a8
canhtrinh <canh.coding@gmail.com>
2016-10-19 10:34:57 -0400

    minor formatting changes to the dashboard.js file

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,93 +1,91 @@
 angular.module('astonishingOwls.search', [])
-.controller('searchCurrency', ['$scope', '$location', 'Search','keysGrabber',
-    function($scope, $location, Search, keysGrabber){
-
-      $scope.availableOptions = {}
-      $scope.listOfCurrency = {};
-      $scope.historyRate = {};
-      $scope.selectedCurrency = '';
-
-      $scope.submitHistoricDate = function(){
-        var getHistoricalInput = $scope.getHistoricalDate;
-        console.log(getHistoricalInput, ' get Historical Input');
-        console.log(typeof getHistoricalInput);
-
-        Search.getHistorical(getHistoricalInput)
-        .then(function(res){
-          console.log(res.rates)
-
-        })
-      }
 
 
-      $scope.getTimeSeries = function(){
-        var userInput = {};
-        userInput.startDates = $scope.timeSeriesStart
-        userInput.endDates = $scope.timeSeriesEnd
-        userInput.symbols = $scope.timeSeriesSymbol
+.controller('searchCurrency', 
+['$scope', '$location', 'Search','keysGrabber','formatDate',
+function($scope, $location, Search, keysGrabber,formatDate){
 
-        Search.getTimeSeries(userInput)
-        .then(function(res){
-          console.log(res)
-        })
-      }
+  $scope.availableOptions = {}
+  $scope.listOfCurrency = {};
+  $scope.historyRate = {};
+  $scope.selectedCurrency = '';
 
+  $scope.getSelectedCurrency = function(){
 
+    var sevenDaysAgo = formatDate(new Date(new Date().setDate(new Date().getDate() - 30)));
+    var thirtyDaysAgo = formatDate(new Date(new Date().setDate(new Date().getDate() - 30)));
+    var yearAgo = formatDate(new Date(new Date().setDate(new Date().getDate() - 365)));
 
-      $scope.getSelectedCurrency = function(){
+    Search.getall().then(function(res){
+      var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
+      $scope.historyRate.todayRate = res.rates[inputCurrency];
+    })
 
-        //format date to YYYY-MM-DD
-        function formatDate(date) {
-          var d = new Date(date),
-              month = '' + (d.getMonth() + 1),
-              day = '' + d.getDate(),
-              year = d.getFullYear();
-          if (month.length < 2) month = '0' + month;
-          if (day.length < 2) day = '0' + day;
+    Search.getHistorical(sevenDaysAgo).then(function(res){
+      var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
+      $scope.historyRate.sevenDaysAgo = res.data.rates[inputCurrency];
+    })
 
-          return [year, month, day].join('-');
-        }
+    Search.getHistorical(thirtyDaysAgo).then(function(res){
+      var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
+      $scope.historyRate.thirtyDaysAgo = res.data.rates[inputCurrency];
+    })
 
-        var sevenDaysAgo = formatDate(new Date(new Date().setDate(new Date().getDate() - 30)));
-        var thirtyDaysAgo = formatDate(new Date(new Date().setDate(new Date().getDate() - 30)));
-        var yearAgo = formatDate(new Date(new Date().setDate(new Date().getDate() - 365)));
+    Search.getHistorical(yearAgo).then(function(res){
+      var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
+      $scope.historyRate.yearAgo = res.data.rates[inputCurrency];
+    })
 
-        Search.getall().then(function(res){
-          var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
-          $scope.historyRate.todayRate = res.rates[inputCurrency];
-        })
+  }
 
-        Search.getHistorical(sevenDaysAgo).then(function(res){
-          var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
-          $scope.historyRate.sevenDaysAgo = res.data.rates[inputCurrency];
-        })
+  //we don't use this yet.... this will be for historical trend analysis
+  $scope.submitHistoricDate = function(){
+    var getHistoricalInput = $scope.getHistoricalDate;
+    Search.getHistorical(getHistoricalInput)
+    .then(function(res){
+    })
+  }
 
-        Search.getHistorical(thirtyDaysAgo).then(function(res){
-          var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
-          $scope.historyRate.thirtyDaysAgo = res.data.rates[inputCurrency];
-        })
+  //we don't use this yet.... this will be for historical trend analysis
+  $scope.getTimeSeries = function(){
+    var userInput = {};
+    userInput.startDates = $scope.timeSeriesStart
+    userInput.endDates = $scope.timeSeriesEnd
+    userInput.symbols = $scope.timeSeriesSymbol
 
-        Search.getHistorical(yearAgo).then(function(res){
-          var inputCurrency = keysGrabber($scope.selectedCurrency, $scope.listOfCurrency)
-          $scope.historyRate.yearAgo = res.data.rates[inputCurrency];
-        })
-
-
-
-      }
-
-
-      //Initializin getall function when page is loaded.
-      Search.getall().then(function(res){
-          // console.log(res.rates, ' GETALL Initialize')
-          $scope.availableOptions.rates = res.rates;
-        })
+    Search.getTimeSeries(userInput)
+    .then(function(res){
+    })
+  }
 
 
-      //Initializing to get all list of currencies
-      Search.getListOfCurrencies().then(function(res){
-        // console.log(res , ' getListOfCurrencies')
-        $scope.listOfCurrency = res
-      })
+  //Initializin getall function when page is loaded.
+  Search.getall().then(function(res){
+    // console.log(res.rates, ' GETALL Initialize')
+    $scope.availableOptions.rates = res.rates;
+  })
 
-    }])
+
+  //Initializing to get all list of currencies
+  Search.getListOfCurrencies().then(function(res){
+    // console.log(res , ' getListOfCurrencies')
+    $scope.listOfCurrency = res
+  })
+
+}])
+
+
+
+.factory('formatDate',function(){
+  //format date to YYYY-MM-DD
+  return function(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [year, month, day].join('-');
+  }
+}); //end of keysGrabber
+
