b513f70c07fda08553c16b855c958097c78a7b3c
canhtrinh <canh.coding@gmail.com>
2016-10-24 11:54:11 -0400

    added api-keys to gitignore

--- server/config/api-key.js
+++ server/config/api-key.js
@@ -1,5 +1,7 @@
-var api_key = 'be114d521d8940e1b03aaed7cc810422';
+var api_key = process.env.api_key || 'be114d521d8940e1b03aaed7cc810422';
 
-module.exports = api_key;
+var giveKey = function(){
+  return api_key;
+};
 
-//want to gitignore this... how do you do that? confirm later
+module.exports = giveKey;
