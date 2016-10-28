e2495f66db243237b08addd1fce2a4224399b9b6
JakeSmith1 <jakesmith5757@gmail.com>
2016-10-28 12:48:35 -0400

    karam file updated with browserify, tests running if chart.js commented out

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration
-// Generated on Wed Oct 26 2016 15:09:52 GMT-0400 (EDT)
+
+//comment out chart.js injecion on line 5 in app.js to get tests to run
 
 module.exports = function(config) {
   config.set({
@@ -7,11 +8,22 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'browserify'],
 
+    plugins : [
+      'karma-mocha',
+      'karma-chai',
+      'karma-browserify',
+      'karma-jasmine',
+      'karma-nyan-reporter',
+      'karma-unicorn-reporter',
+      'karma-phantomjs-launcher',
+      'karma-babel-preprocessor',
+      'karma-spec-reporter',
+      'karma-mocha-reporter'
+    ],
 
     // list of files / patterns to load in the browser
     files: [
@@ -21,7 +33,9 @@ module.exports = function(config) {
     'client/lib/angular-mocks/angular-mocks.js',
 
     // find other sources for tests
+    'client/lib/chart.js/src/**/*.js',
     'client/lib/chart.js/src/chart.js',
+    'client/lib/angular-ui-grid/ui-grid.js',
 
     // our app code
     'client/app/**/*.js',
@@ -30,27 +44,42 @@ module.exports = function(config) {
     'specs/client/routesTest.js'
     ],
 
-
     // list of files to exclude
     exclude: [
+        'client/lib/angular/angular-loader.js',
+        'client/lib/angular/*.min.js',
+        'client/lib/angular/angular-scenario.js'
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    },
+     'client/**/*.js': ['babel'],
+     'spec/client/*.js': ['babel'],
+     'client/lib/chart.js/src/chart.js': ['browserify'],
+     'client/lib/chart.js/src/**/*.js': ['browserify']
+   },
+   babelPreprocessor: {
+     options: {
+       presets: ['es2015'],
+       sourceMap: 'inline'
+     },
+   },
 
-    // test results reporter to use
-    reporters: ['nyan', 'unicorn'],
+    browserify: {
+     debug: true
+   },
 
-    // start these browsers. PhantomJS will load up in the background
-    browsers: ['PhantomJS'],
+  // test results reporter to use
+  reporters: ['nyan', 'mocha'],
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+  // start these browsers. PhantomJS will load up in the background
+  browsers: ['PhantomJS'],
 
-    // if true, Karma exits after running the tests.
-    singleRun: true
+  // enable / disable watching file and executing tests whenever any file changes
+  autoWatch: false,
+
+  // if true, Karma exits after running the tests.
+  singleRun: true
   });
 };
