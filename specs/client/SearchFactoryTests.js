9dbfba83aa1c1b482841fd0fdc824e6d515a0ace
JakeSmith1 <jakesmith5757@gmail.com>
2016-10-28 19:02:26 -0400

    test specs for Search Factory

---
+++ specs/client/SearchFactoryTests.js
@@ -0,0 +1,31 @@
+describe('Factory', function () {
+  beforeEach(module('astonishingOwls.factory'));
+
+  afterEach(inject(function ($httpBackend) {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  }));
+
+
+describe('Search Factory', function () {
+    var Search;
+
+    beforeEach(inject(function (_Search_) {
+      Search = _Search_;
+    }));
+
+    it('should exist', function () {
+      expect(Search).to.exist;
+    });
+
+    it('should have a method `getDB`', function () {
+      expect(Search.getDB).to.be.a('function');
+    });
+
+    it('should have a method `postDB`', function () {
+      expect(Search.postDB).to.be.a('function');
+    });
+
+  });
+
+});
